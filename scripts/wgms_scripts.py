70ccd39c30e9a72340b59cf1f8bb60b9104331e0
windnage <windnage@hotmail.com>
2019-05-03 02:25:43 -0600

    Added 5 functions - multi_temporal_glims, find_glacier_all_glims, find_glacier_clean_glims, ten_largest, save_3_largest

--- scripts/wgms_scripts.py
+++ scripts/wgms_scripts.py
@@ -14,6 +14,7 @@ It currently contains 4 functions:
 
 import geopandas as gpd
 import pandas as pd
+import os
 
 
 def open_rgi_region(region_no):
@@ -55,13 +56,39 @@ def open_rgi_region(region_no):
                              "19_rgi60_AntarcticSubantarctic/19_rgi60_AntarcticSubantarctic.shp"]
 
         # Open file 
-        print(region_file_names[region_no-1])
+        #print(region_file_names[region_no-1])
         rgi_region_df = gpd.read_file(root_data_dir + region_file_names[region_no-1])
     else:
         rgi_region_df = "-999"
         print("Specified region does not exist.")
     
     return rgi_region_df
+
+
+def open_clean_glims(region_no):
+    '''
+    Opens cleaned GLIMS shapefile for one of 19 glacial regions
+
+    Parameters
+    ----------
+    region_no : The region number as an integer. Accepted values are 1 through 19.
+
+    Returns
+    ----------
+    glims_region_df: Returns a geopandas dataframe of the shapefile for given region.
+    '''
+    
+    if region_no >= 1 and region_no <=19:        
+        
+        # Open file
+        glims_fp = "data/glims/processed/cleaned/glims_region_" + str(region_no) + "_cleaned.shp" 
+        glims_region_df = gpd.read_file(glims_fp)
+        
+    else:
+        glims_region_df = "-999"
+        print("Specified region does not exist.")
+    
+    return glims_region_df    
 
 
 
@@ -96,7 +123,7 @@ def split_glims(data, all_regions, region_name, fp):
     Parameters
     ----------
     data : Geodataframe containing polygons of all the GLIMS data
-    regions : Geodataframe containing a single polygon of the region you want to check the GLIMS data against.
+    all_regions : Geodataframe containing outlines of the 19 glacier regions.
     region_name : String containing the name of the region
     fp : String containing the file path to the location where the region shapefile should be saved.
 
@@ -114,6 +141,10 @@ def split_glims(data, all_regions, region_name, fp):
 
     # Pass pip_mask into data to get the ones that are in the specified region
     glims_region = data.loc[pip_mask]
+    
+    print(region.RGI_CODE[0])
+    
+    glims_region.insert(0, 'region_no', region.RGI_CODE[0])
 
     # Save regional dataframe to shapefile
     glims_region.to_file(driver='ESRI Shapefile', filename=fp)
@@ -127,7 +158,7 @@ def clean_glims(region_glims, fp):
 
     Parameters
     ----------
-    region_glims : Geodataframe containing polygons for one regions of GLIMS data
+    region_glims : Geodataframe containing polygons for one region of GLIMS data
     fp : String containing the file path to the location where the region shapefile should be saved.
 
     Returns
@@ -165,7 +196,7 @@ def clean_glims(region_glims, fp):
     
     return
 
-def print_10_largest_glims(region_no):
+def print_10_largest_glims(region_no, do_print=None):
     """
     Opens and prints the list of 10 largest glaciers for a specified region for GLIMS and
     returns the data as a pandas dataframe.
@@ -173,11 +204,15 @@ def print_10_largest_glims(region_no):
     Parameters
     ----------
     region_no : The region number as an integer. Accepted values are 1 through 19.
+    do_print : String, if set to "false", will not print. Default is to print.
 
     Returns
     -------
     glims_largest : A pandas dataframe with a list of the 10 largest glaciers for the specified region
     """
+    
+    # Check do_print. If not set, set to true
+    do_print = do_print or "true"
     
     # Create a list with all the region names
     region_names = ["Alaska", "Western Canada and USA",
@@ -191,14 +226,15 @@ def print_10_largest_glims(region_no):
     # Open GLIMS csv file for specified region with 10 largest glaciers
     glims_largest_fp = "data/glims/processed/largest/glims_region_" + str(region_no) + "_largest.csv"
     glims_largest = pd.read_csv(glims_largest_fp)
-    print('GLIMS 10 Largest glaciers and their size for Region ' + str(region_no) + ' - ' + region_names[region_no-1] + ':')
-    print('')
-    print('      Glacier ID               Area (km^2)      Glacier Name       Date of Measurement')
-    print(glims_largest.to_string(header=False, index=False, col_space=20))
+    if do_print != "false":
+        print('GLIMS 10 Largest glaciers and their size for Region ' + str(region_no) + ' - ' + region_names[region_no-1] + ':')
+        print('')
+        print('      Glacier ID               Area (km^2)      Glacier Name       Date of Measurement')
+        print(glims_largest.to_string(header=False, index=False, col_space=20))
     
     return glims_largest
 
-def print_10_largest_rgi(region_no):
+def print_10_largest_rgi(region_no, do_print=None):
     """
     Opens and prints the list of 10 largest glaciers for a specified region for RGI and
     returns the data as a pandas dataframe.
@@ -206,11 +242,15 @@ def print_10_largest_rgi(region_no):
     Parameters
     ----------
     region_no : The region number as an integer. Accepted values are 1 through 19.
+    do_print : String, if set to "false", will not print. Default is to print.
 
     Returns
     -------
     rgi_largest : A pandas dataframe with a list of the 10 largest glaciers for the specified region
     """
+
+    # Check do_print. If not set, set to true
+    do_print = do_print or "true"
     
     # Create a list with all the region names
     region_names = ["Alaska", "Western Canada and USA",
@@ -224,9 +264,185 @@ def print_10_largest_rgi(region_no):
     # Open RGI csv file for specified region with 10 largest glaciers
     rgi_largest_fp = "data/rgi/processed/largest/rgi_region_" + str(region_no) + "_largest.csv"
     rgi_largest = pd.read_csv(rgi_largest_fp)
-    print('RGI 10 Largest glaciers and their size for Region ' + str(region_no) + ' - ' + region_names[region_no-1] + ':')
-    print('')
-    print('      Glacier ID               Area (km^2)      Glacier Name       Date of Measurement')
-    print(rgi_largest.to_string(header=False, index=False, col_space=20))
+    
+    if do_print != "false":
+        print('RGI 10 Largest glaciers and their size for Region ' + str(region_no) + ' - ' + region_names[region_no-1] + ':')
+        print('')
+        print('      Glacier ID               Area (km^2)      Glacier Name       Date of Measurement')
+        print(rgi_largest.to_string(header=False, index=False, col_space=20))
     
     return rgi_largest
+
+def multi_temporal_glims(region_no, do_print=None):
+    """
+    Finds all the dates that the largest 3 glaciers have measurements for for each of the 19 regions from GLIMS.
+
+    Parameters
+    ----------
+    region_no : The region number as an integer. Accepted values are 1 through 19.
+    do_print : String, if set to "false", will not print. Default is to print.
+
+    Returns
+    -------
+    glims_multi_temporal_largest : A pandas dataframe with a list of the 3 largest glaciers for the specified region
+                                 along with all the different dates that they have measurements.
+    """
+    
+    # Open GLIMS region shapefile
+    glims_region_fp = "data/glims/processed/glims_region_" + str(region_no) + ".shp"
+    glims_polygons = gpd.read_file(glims_region_fp)
+
+    # Open and print GLIMS Region csv file with 10 largest glaciers
+    glims_largest = print_10_largest_glims(region_no, do_print="false")
+    
+    # Find all the instances for when the top 3 largest glaciers occur
+    largest_1 = glims_polygons[(glims_polygons['glac_id'] == glims_largest.glac_id[0]) &
+                (glims_polygons['line_type'] == "glac_bound")]
+
+    largest_2 = glims_polygons[(glims_polygons['glac_id'] == glims_largest.glac_id[1]) &
+                (glims_polygons['line_type'] == "glac_bound")]
+
+
+    largest_3 = glims_polygons[(glims_polygons['glac_id'] == glims_largest.glac_id[2]) &
+                (glims_polygons['line_type'] == "glac_bound")]
+
+    # Concantenate them all together
+    glims_multi_temporal_largest = pd.concat([largest_1, largest_2, largest_3])
+                              
+    if do_print != "false":
+        print(largest_1[['glac_name', 'glac_id', 'src_date', 'db_area']])
+        print('')
+        
+        print(largest_2[['glac_name', 'glac_id', 'src_date', 'db_area']])
+        print('')
+        
+        print(largest_3[['glac_name', 'glac_id', 'src_date', 'db_area']])
+        print('')
+    
+    return glims_multi_temporal_largest
+
+def find_glacier_all_glims(glims_id, region_no):
+    """
+    Extract the data rows for a particular glacier from the full GLIMS database, which contains all temporal
+    measurements.
+
+    Parameters
+    ----------
+    glims_id : String containing the GLIMS glacier id.
+    region_no : The region number as an integer. Accepted values are 1 through 19. This number is required 
+                to open the correct data file.
+
+    Returns
+    -------
+    glims_data : A pandas dataframe with a the rows of data associated with the given glims id.
+    """
+    
+    # Open the regional GLIMS file
+    glims_glacier_data_fp = "data/glims/processed/glims_region_" + str(region_no) + ".shp"
+    glims_glacier_data = gpd.read_file(glims_glacier_data_fp)
+    
+    # Find the glacier based on the GLIMS Id
+    glims_glacier = glims_glacier_data[glims_glacier_data['glac_id']==glims_id]
+    
+    return glims_glacier
+
+def find_glacier_clean_glims(glims_id, region_no):
+    """
+    Extract the data rows for a particular glacier from the cleaned GLIMS database, which contains only the latest
+    measurements.
+
+    Parameters
+    ----------
+    glims_id : String containing the GLIMS glacier id.
+    region_no : The region number as an integer. Accepted values are 1 through 19. This number is required 
+                to open the correct data file.
+
+    Returns
+    -------
+    glims_data : A pandas dataframe with a the rows of data associated with the given glims id.
+    """
+    
+    # Open the regional GLIMS file
+    glims_glacier_data_fp = "data/glims/processed/cleaned/glims_region_" + str(region_no) + "_cleaned.shp"
+    glims_glacier_data = gpd.read_file(glims_glacier_data_fp)
+    
+    # Find the glacier based on the GLIMS Id
+    glims_glacier = glims_glacier_data[glims_glacier_data['glac_id']==glims_id]
+    
+    return glims_glacier
+
+def ten_largest(data, region_no, source):
+    '''
+    Finds the 10 largest glaciers in a region and saves them to a csv file
+
+    Parameters
+    ----------
+    data : Geodataframe containing all glacier polygons for a region
+    region_no : Integer with the region number. Accepted values are 1 through 19.
+    source :  String with the source of the glacier outlines. Accepted values are GLIMS or RGI
+
+    Returns
+    ----------
+    nothing: Saves a csv file of the 10 largest glaciers for a region
+    '''
+    
+    if source == 'GLIMS':
+        # Find 10 largest
+        ten_largest_df = data[['glac_id', 'db_area', 'glac_name', 'src_date']].nlargest(10, 'db_area')
+        
+        # Save to csv file if it doesn't already exist
+        glims_largest_csv_fp = "data/glims/processed/largest/glims_region_" + str(region_no) + "_largest.csv"
+        if os.path.exists(glims_largest_csv_fp) == False:
+            print(region_no)
+            ten_largest_df.to_csv(glims_largest_csv_fp, index=False)
+        
+    elif source == 'RGI':
+        # Find 10 largest
+        ten_largest_df = data[['GLIMSId', 'Area', 'Name', 'BgnDate']].nlargest(10, 'Area')
+        
+        # Save to csv file if it doesn't already exist
+        rgi_largest_csv_fp = "data/rgi/processed/largest/rgi_region_" + str(region_no) + "_largest.csv"
+        if os.path.exists(rgi_largest_csv_fp) == False:
+            print(region_no)
+            ten_largest_df.to_csv(rgi_largest_csv_fp, index=False)
+        
+    else:
+        print("Incorrect source input")
+    
+    return
+
+def save_3_largest(largest_1_df, largest_2_df, largest_3_df, region_no, source):
+    '''
+    Saves the 3 largest GLIMS glacier outlines in a region to a shapefile
+
+    Parameters
+    ----------
+    largest_1_df : Geodataframe containing the first largest glacier polygon for a region
+    largest_2_df : Geodataframe containing the second largest glacier polygon for a region
+    largest_3_df : Geodataframe containing the third largest glacier polygon for a region
+    region_no : Integer with the region number. Accepted values are 1 through 19
+    source :  String with the source of the glacier outlines. Accepted values are GLIMS or RGI
+
+    Returns
+    ----------
+    nothing: Saves a shapefile 3 largest GLIMS glaciers for a region
+    '''    
+    
+    # Set file path based on source selected
+    if source == 'GLIMS':
+        largest_3_fp = "data/glims/processed/largest/glims_region_" + str(region_no) + "_largest.shp"
+    elif source == 'RGI':
+        largest_3_fp = "data/rgi/processed/largest/rgi_region_" + str(region_no) + "_largest.shp"
+    else:
+        print("Incorrect source input")
+        return
+    
+    # Check if the file already exists; if it does not, save file.
+    if os.path.exists(largest_3_fp) == False:
+        # Concatenate the 3 biggest into one dataframe
+        largest_3 = gpd.GeoDataFrame(pd.concat([largest_1_df, largest_2_df, largest_3_df]))
+
+        # Save 3 largest from specified region to shapefile
+        largest_3.to_file(driver='ESRI Shapefile', filename=largest_3_fp)
+    
+    return
