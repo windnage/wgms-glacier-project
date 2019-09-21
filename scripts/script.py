c65be603c28853c8043877553f47be0d8593b435
windnage <windnage@hotmail.com>
2019-09-20 18:21:15 -0600

    made minor edits to run with python 3.7

---
+++ scripts/script.py
@@ -0,0 +1,30 @@
+import fiona
+from shapely.ops import cascaded_union
+from shapely.geometry import shape, mapping
+
+src = '/tmp/polys/glims_region_7_cleaned.shp'
+
+with fiona.open(src, 'r') as ds_in:
+    crs = ds_in.crs
+    drv = ds_in.driver
+
+    geoms = []
+    for x in ds_in:
+        geom = shape(x["geometry"])
+        if not geom.is_valid:
+            geom = geom.buffer(0)
+
+        geoms.append(geom)
+
+    dissolved = cascaded_union(geoms)
+
+schema = {
+    "geometry": "Polygon",
+    "properties": {"id": "int"}
+}
+
+dst = '/tmp/polys/soutput.shp'
+
+with fiona.open(dst, 'w', driver=drv, schema=schema, crs=crs) as ds_dst:
+    for i, g in enumerate(dissolved):
+        ds_dst.write({"geometry": mapping(g), "properties": {"id": i}})
